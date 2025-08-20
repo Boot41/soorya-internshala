0dfade4a04455b82d347b4a070dd723f87f4cf43
Soorya U <soorya.u@think41.com>
2025-08-20 14:17:37 +0530

    chore: minor fixes

--- client/web/src/routes/auth/sign-up.tsx
+++ client/web/src/routes/auth/sign-up.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Link } from '@tanstack/react-router'
 import { GalleryVerticalEnd } from "lucide-react"
 import { SignUpForm } from "@/components/sign-up-form"
 import { AuthLayout } from "@/layouts/auth-layout"
@@ -12,12 +12,12 @@ export default function SignUpPage() {
     <AuthLayout>
       <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
         <div className="flex w-full max-w-sm flex-col gap-6">
-          <a href="#" className="flex items-center gap-2 self-center font-medium">
+          <Link to="/" className="flex items-center gap-2 self-center font-medium">
             <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
               <GalleryVerticalEnd className="size-4" />
             </div>
-            Acme Inc.
-          </a>
+            Internshala
+          </Link>
           <SignUpForm />
         </div>
       </div>
